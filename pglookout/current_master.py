fc639c6058716668f21da29e1d4fc9ad29a74761
Hannu Valtonen <hannu.valtonen@f-secure.com>
2014-09-11 18:56:52 +0300

    current_master: If json state file is too old exit with error
    Otherwise we might be returning stale information.

--- pglookout/current_master.py
+++ pglookout/current_master.py
@@ -1,6 +1,7 @@
 import json
 import os
 import sys
+import time
 
 def main():
     if len(sys.argv) != 2:
@@ -11,6 +12,9 @@ def main():
     try:
         config = json.loads(open(sys.argv[1], "r").read())
         state_file_path = config.get("json_state_file_path", "/tmp/json_state_file")
+        if time.time() - os.stat(state_file_path).st_mtime > 60.0:
+            # file older than one minute, pglookout probably dead, exit with minus one
+            sys.exit(-1)
         state_dict = json.loads(open(state_file_path, "r").read())
         current_master = state_dict['current_master']
         print(current_master)
