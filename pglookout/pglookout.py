Aiven-Open/pglookout

commit 6e4ce4db83dee270d696b72c0b86304eaacaa83b

pglookout: pass sys.argv to main() to allow overriding it
This makes it a bit easier to execute the main() functionality from external
programs as they don't need to override sys.argv.

pglookout/pglookout.py
@@ -632,13 +632,13 @@ class ClusterMonitor(Thread):
                 self.log.exception("Problem in ClusterMonitor")
             time.sleep(self.config.get("db_poll_interval", 5.0))
 
-def main():
-    if len(sys.argv) == 2 and os.path.exists(sys.argv[1]):
-        pglookout = PgLookout(sys.argv[1])
+def main(args):
+    if len(args) == 1 and os.path.exists(args[0]):
+        pglookout = PgLookout(args[0])
         pglookout.run()
     else:
         print("Usage, pglookout <config filename>")
-        sys.exit(0)
+        return 1
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main(sys.argv[1:]))

pglookout/current_master.py
@@ -10,24 +10,23 @@ import os
 import sys
 import time
 
-def main():
-    if len(sys.argv) != 2:
+def main(args):
+    if len(args) != 1:
         print("Usage, pglookout_current_master <path_to_pglookout.json>")
-        sys.exit(-1)
-    if not os.path.exists(sys.argv[1]):
-        sys.exit(-1)
+        return -1
+    if not os.path.exists(args[0]):
+        return -1
     try:
-        config = json.loads(open(sys.argv[1], "r").read())
+        config = json.loads(open(args[0], "r").read())
         state_file_path = config.get("json_state_file_path", "/tmp/pglookout_state.json")
         if time.time() - os.stat(state_file_path).st_mtime > 60.0:
             # file older than one minute, pglookout probably dead, exit with minus one
-            sys.exit(-1)
+            return -1
         state_dict = json.loads(open(state_file_path, "r").read())
         current_master = state_dict['current_master']
         print(current_master)
     except:
-        sys.exit(-1)
-    sys.exit(0)
+        return -1
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main(sys.argv[1:]))

commit f7cd0c2e6479c1224b68425ec27dbcfedc99aac3

pglookout: Fix main() setup.py entrypoint broken in 6e4ce4db
The main() entrypoint is used from setup.py for the pglookout
entrypoint, and cannot take unconditional params.

pglookout/pglookout.py
@@ -633,7 +633,9 @@ class ClusterMonitor(Thread):
                 self.log.exception("Problem in ClusterMonitor")
             time.sleep(self.config.get("db_poll_interval", 5.0))
 
-def main(args):
+def main(args=None):
+    if not args:
+        args = sys.argv[1:]
     if len(args) == 1 and os.path.exists(args[0]):
         pglookout = PgLookout(args[0])
         pglookout.run()