cc698a6ece2a29f7ca3b22c5f78747a2b19e69a0
Oskari Saarenmaa <os@ohmu.fi>
2017-01-23 15:39:54 +0200

    tests: use pytest and fixtures instead of unittest.TestCase
    Also try to make sure we shut down the webserver at the end of each test
    case.

--- test/conftest.py
+++ test/conftest.py
@@ -4,8 +4,27 @@ pglookout - test configuration
 Copyright (c) 2016 Ohmu Ltd
 See LICENSE for details
 """
-
+try:
+    from mock import Mock  # pylint: disable=import-error
+except ImportError:  # py3k import location
+    from unittest.mock import Mock  # pylint: disable=import-error,no-name-in-module
 from pglookout import logutil
+from pglookout.pglookout import PgLookout
+import pytest
 
 
 logutil.configure_logging()
+
+
+@pytest.yield_fixture
+def pgl():
+    pgl_ = PgLookout("pglookout.json")
+    pgl_.check_for_maintenance_mode_file = Mock()
+    pgl_.check_for_maintenance_mode_file.return_value = False
+    pgl_.cluster_monitor._connect_to_db = Mock()  # pylint: disable=protected-access
+    pgl_.create_alert_file = Mock()
+    pgl_.execute_external_command = Mock()
+    try:
+        yield pgl_
+    finally:
+        pgl_.quit()
--- test/test_webserver.py
+++ test/test_webserver.py
@@ -15,8 +15,10 @@ def test_webserver():
     config = {}
     cluster_state = {"hello": 123}
     web = WebServer(config=config, cluster_state=cluster_state)
-    web.start()
-    time.sleep(1)
-    result = requests.get("http://127.0.0.1:15000/state.json").json()
-    assert result == cluster_state
-    web.close()
+    try:
+        web.start()
+        time.sleep(1)
+        result = requests.get("http://127.0.0.1:15000/state.json").json()
+        assert result == cluster_state
+    finally:
+        web.close()
