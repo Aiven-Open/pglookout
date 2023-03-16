0d4ea2c5ebab5175f2a948c39dfd9bd572e6b7e4
Samuel Giffard <samuel.giffard@aiven.io>
2023-03-16 15:52:41 +0100

    mypy: `pglookout/logutil.py` [BF-1560]

--- pglookout/logutil.py
+++ pglookout/logutil.py
@@ -5,39 +5,50 @@ pglookout - logging formats and utility functions
 Copyright (c) 2015 Ohmu Ltd
 See LICENSE for details
 """
+from __future__ import annotations
+
+from logging.handlers import SysLogHandler
+from typing import Final, TYPE_CHECKING
 
 import logging
-import logging.handlers
 import os
 
+with_systemd: bool = False
 try:
     from systemd import daemon  # pylint: disable=no-name-in-module
+
+    with_systemd = True
 except ImportError:
-    daemon = None
+    if not TYPE_CHECKING:
+        daemon = None
 
-LOG_FORMAT = "%(asctime)s\t%(name)s\t%(threadName)s\t%(levelname)s\t%(message)s"
-LOG_FORMAT_SHORT = "%(levelname)s\t%(message)s"
-LOG_FORMAT_SYSLOG = "%(name)s %(threadName)s %(levelname)s: %(message)s"
+LOG_FORMAT: Final[str] = "%(asctime)s\t%(name)s\t%(threadName)s\t%(levelname)s\t%(message)s"
+LOG_FORMAT_SHORT: Final[str] = "%(levelname)s\t%(message)s"
+LOG_FORMAT_SYSLOG: Final[str] = "%(name)s %(threadName)s %(levelname)s: %(message)s"
 
 
-def set_syslog_handler(address, facility, logger):
-    syslog_handler = logging.handlers.SysLogHandler(address=address, facility=facility)
+def set_syslog_handler(address: str, facility: str | int, logger: logging.Logger) -> SysLogHandler:
+    if isinstance(facility, str):
+        facility_id: int = SysLogHandler.facility_names.get(facility, SysLogHandler.LOG_LOCAL2)
+    else:
+        facility_id = facility
+    syslog_handler = SysLogHandler(address=address, facility=facility_id)
     logger.addHandler(syslog_handler)
     formatter = logging.Formatter(LOG_FORMAT_SYSLOG)
     syslog_handler.setFormatter(formatter)
     return syslog_handler
 
 
-def configure_logging(level=logging.DEBUG, short_log=False):
+def configure_logging(level: int = logging.DEBUG, short_log: bool = False) -> None:
     # Are we running under systemd?
     if os.getenv("NOTIFY_SOCKET"):
         logging.basicConfig(level=level, format=LOG_FORMAT_SYSLOG)
-        if not daemon:
+        if not with_systemd:
             print("WARNING: Running under systemd but python-systemd not available, systemd won't see our notifications")
     else:
         logging.basicConfig(level=level, format=LOG_FORMAT_SHORT if short_log else LOG_FORMAT)
 
 
-def notify_systemd(status):
-    if daemon:
+def notify_systemd(status: str) -> None:
+    if with_systemd:
         daemon.notify(status)
